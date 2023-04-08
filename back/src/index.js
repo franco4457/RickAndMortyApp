bfa9cea3bc16a2b148d170f0ad5b6fb2c950b168
franco4457 <franco_forex@hotmail.com>
2023-04-08 13:39:53 -0300

    change forceXalter

--- back/src/index.js
+++ back/src/index.js
@@ -2,7 +2,7 @@ const app = require("./app");
 const PORT =process.env.SV_PORT ||3001;
 const {sequelize} = require("./db/index");
 
-sequelize.sync({force:true}).then(() => {
+sequelize.sync({alter:true}).then(() => {
     app.listen(PORT,() => {
         console.log(`listening on port ${PORT}`);
     })
